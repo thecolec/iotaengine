51cc56267b6d9352861c180a0b85183500439ab4
Cole Cassidy <colewcassidy@gmail.com>
2022-03-03 10:18:12 -0500

    API Expansion
    Created Separate module for application logic.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,39 @@
+// INDEX.JS
+// Cole Cassidy - 2022
+// Express API router.
+
+// external libraries
 var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000;
 var MongoClient = require('mongodb').MongoClient;
 var dburl = 'mongodb://localhost/iotattestingdb';
 
+// internal libraries
+var iota = require('./iota.js');
+
 app.get('/', (req, res) => {
     res.send("Hello World");
     console.log("Request Received");
 });
 
+// Authentication API
 app.get('/auth', (req, res) => {
     res.send("Future Authentication Endpoint")
     console.log("Auth Request Received");
 });
 
+// User API
 app.get('/user', (req, res) => {
     res.send("Future User Endpoint");
     console.log("User Request Received");
 });
+app.get('/user/add', (req, res) => {
+    iota.createuser("add");
+    res.send("Added")
+})
 
+// Organization API
 app.get('/org', (req, res) => {
     res.send("Future Organization Endpoint");
     console.log("Organization Request Received");
---
+++ src/iota.js
@@ -0,0 +1,7 @@
+// IOTA.JS
+// Cole Cassidy - 2022
+// Primary application logic.
+
+exports.createuser = (msg) => {
+    console.log(msg);
+}
